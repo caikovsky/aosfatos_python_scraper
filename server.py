5f7ae034c875d04926cceb049157efccaed3e073
caikovsky <mauranocaique@gmail.com>
2019-08-19 15:34:29 -0700

    flask api to run spider

---
+++ server.py
@@ -0,0 +1,27 @@
+import subprocess
+import json, re, os
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__) #init Flask
+app.config['JSON_AS_ASCII'] = False #force UTF-8 encoding to jsonify
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}}) # enable CORS
+
+@app.route('/api/crawl')
+def index():
+  data = '' # "None"
+
+  if os.path.isfile('./fatos_encoded2.json'):
+    with open("fatos_encoded.json") as f:
+      data = jsonify(json.loads(f.read()))
+  else:
+    spider_name = "fatos"
+    subprocess.check_output(['scrapy', 'crawl', spider_name, "-o", "fatos.json", "-s", "HTTPCACHE_ENABLED=1"])
+
+    with open("fatos.json") as f:
+      data = jsonify(json.loads(f.read()))
+
+  return data
+
+if __name__ == '__main__':
+  app.run(debug=True)
